e6467bdbe9d0bf5072b316f5612911d281211576
James Fogarty <jayfo.github@gmail.com>
2014-09-05 07:06:56 -0700

    Update site title and deployment info

--- fabfile.py
+++ fabfile.py
@@ -10,23 +10,21 @@ def build():
 
 
 def deploy():
-    pass
-'''
     # Locally build the website
     fabric.api.local('jekyll build --config _config.yml')
 
     # Ensure the server has our staging directory
-    fabric.api.run('mkdir -p ~/fabric_staging/web-jayfo/')
+    fabric.api.run('mkdir -p ~/fabric_staging/web-cse440-au14/')
 
     # Ensure the staging directory is empty
-    fabric.api.run('rm -rf ~/fabric_staging/web-jayfo/*')
+    fabric.api.run('rm -rf ~/fabric_staging/web-cse440-au14/*')
 
     # Push up to the server staging directory
-    fabric.api.put('_site/*', '~/fabric_staging/web-jayfo/')
+    fabric.api.put('_site/*', '~/fabric_staging/web-cse440-au14/')
 
     # And sync into the deployment directory
-    fabric.api.run('rsync -r -c --delete ~/fabric_staging/web-jayfo/ ~/public_html/')
-'''
+    fabric.api.run('rsync -r -c --delete ~/fabric_staging/web-cse440-au14/ /cse/web/courses/cse440/14au/')
+
 
 def serve():
     fabric.api.local('jekyll serve --config _config.yml,_config-dev.yml --watch --force_polling')
